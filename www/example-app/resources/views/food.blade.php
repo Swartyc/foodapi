7b933af8ba7d4634675d92f315382cb6cca1bca3
Emmanuel <90639619+Swartyc@users.noreply.github.com>
2022-02-04 14:22:00 +0100

    fix grid

--- www/example-app/resources/views/food.blade.php
+++ www/example-app/resources/views/food.blade.php
@@ -11,7 +11,10 @@
                 grid-template-columns: repeat(auto-fill, 230px);   
                 grid-gap: 32px;   
                 padding-top: 64px;
-            }    
+            }  
+            .max-imag{
+                max-width: 230px;
+            }  
         }
         
         
@@ -20,7 +23,7 @@
 </head>
 <body>
     <h1 class="py-4 text-6xl font-bold text-center text-green-800">Find recipe ideas</h1>
-    <form action="{{route('food.search')}}" class="flex justify-center py-16 test-xl" method="post">
+    <form action="{{route('food.search')}}" class="flex justify-center flex-wrap py-16 test-xl" method="post">
         <input class="border-4 border-green-600 rounded-lg bg-green-500 placeholder-green-900 px-4 py-1 hover:cursor-text" name="word" type="text" placeholder="Pasta">
         <div class="border-2 border-green-600 rounded-lg bg-green-500 text-black px-4 py-1 mx-8">
             Number :
@@ -44,12 +47,12 @@
         document.getElementById('search-bar').value = "<?php echo $_POST['number_choose'];?>";
     </script>
 
-    <div class="grid lg:column-auto-fill justify-center">
+    <div class="grid column-auto-fill px-12 justify-center">
         <?php $numbers = $food["number"]; ?>
 
         @for ($i = 0; $i < $numbers; $i++)
         <?php $id_details=$food["results"][$i]["id"]; ?>
-            <a href="{{route('food.details',[$id_details])}}" >
+            <a class="max-imag" href="{{route('food.details',[$id_details])}}" >
                 <img src='{{$food["results"][$i]["image"]}}' alt='{{$food["results"][$i]["title"]}}_image'/>
                 <legend>{{$food["results"][$i]["title"]}}</legend>
             </a>
